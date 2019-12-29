cdfa77e68821f5f9fe3dce9b122c476a0f5cfbc0
Cyhyraeth <maboose@yahoo.com>
2019-12-29 15:20:30 -0800

    Commit after tutorial #14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import Ninjas from "./Ninjas";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first React app!</h1>
-      <p>Welcome :)</p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Ryu", age: 30, belt: "Black", id: 1 },
+      { name: "Yoshi", age: 20, belt: "Green", id: 2 },
+      { name: "Crystal", age: 25, belt: "Pink", id: 3 }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    );
+  }
 }
 
 export default App;
